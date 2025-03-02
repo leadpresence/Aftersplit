27b3285b1aec9816c71cc3812328bc8632da2b55
leadpresence <noblefelix4@gmail.com>
2025-03-02 08:35:19 +0100

    Initial Commit

---
+++ AfterSplitTests/AfterSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfterSplitTests.swift
+//  AfterSplitTests
+//
+//  Created by Kehinde Akeredolu on 02/03/2025.
+//
+
+import Testing
+@testable import AfterSplit
+
+struct AfterSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
